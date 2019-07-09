8da33374786f22d7d4fcab9af17f6bbd32b181b3
TyK <tisyang@gmail.com>
2019-07-09 09:17:04 +0800

    update log.h and wsocket

--- ntripcaster.c
+++ ntripcaster.c
@@ -1,8 +1,75 @@
-#include "log.h"
+#include <time.h>
+#include "logh/log.h"
 #include "wsocket/wsocket.h"
+
+
+static wsocket listen_on(const char *addr, const char* service)
+{
+    wsocket sock = INVALID_WSOCKET;
+
+    struct addrinfo hints = {0};
+    hints.ai_family = PF_UNSPEC;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    int rv = 0;
+    struct addrinfo *ai = NULL;
+    if ((rv = getaddrinfo(addr, service, &hints, &ai)) != 0) {
+        LOG_ERROR("getaddrinfo() error, %s", gai_strerror(rv));
+        return INVALID_WSOCKET;
+    }
+    for (const struct addrinfo *p = ai; p != NULL; p = p->ai_next) {
+        sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sock == INVALID_WSOCKET) {
+            continue;
+        }
+        // enable addr resuse
+        setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
+        if (bind(sock, p->ai_addr, p->ai_addrlen) == WSOCKET_ERROR) {
+            // bind error
+            wsocket_close(sock);
+            sock = INVALID_WSOCKET;
+            continue;
+        }
+        // Got it!
+        break;
+    }
+
+    if (sock == INVALID_WSOCKET) {
+        LOG_ERROR("socket() or bind() error, %s", wsocket_strerror(wsocket_errno));
+        freeaddrinfo(ai);
+        ai = NULL;
+        return INVALID_WSOCKET;
+    }
+
+    freeaddrinfo(ai);
+    ai = NULL;
+
+    if (listen(sock, 2) == WSOCKET_ERROR) {
+        LOG_ERROR("listen() error, %s", wsocket_strerror(wsocket_errno));
+        wsocket_close(sock);
+        return INVALID_WSOCKET;
+    }
+
+    return sock;
+}
 
 int main(int argc, const char *argv[])
 {
-    LOG_DEBUG("hello world");
+    LOG_DEBUG("Debug %d", 1);
+    LOG_TRACE("Trace %s", "abc");
+    LOG_INFO("Info");
+    LOG_WARN("Warn");
+    LOG_ERROR("Error");
+    WSOCKET_INIT();
+    wsocket fd = listen_on(NULL, "1024");
+    if (fd == INVALID_WSOCKET) {
+        LOG_ERROR("setup server error.");
+    } else {
+        LOG_INFO("setup server on 1024 OK.");
+    }
+    sleep(3);
+    WSOCKET_CLEANUP();
     return 0;
 }
